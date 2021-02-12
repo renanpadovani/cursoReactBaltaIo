4247121362db2926e4d5b6adb7d9157b9a3bcbaf
Renan Padovani <renan_padovani@hotmail.com>
2021-02-12 17:18:42 -0300

    Adicionando projeto concluído

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useContext } from 'react';
+import { TodoContext } from '../contexts/TodoContext';
+import { TodoContextType } from '../contexts/TodoContextType';
+import TodoListItem from './TodoListItem';
+
+const TodoList = () => {
+
+    const { todos } = useContext<TodoContextType>(TodoContext)
+
+    return (
+        <div>
+            <table className="uk-table">
+                <caption>Lista de tarefas</caption>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Tarefa</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        todos?.map(
+                            todo => (<TodoListItem key={todo.id} todo={todo} />)
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default TodoList
